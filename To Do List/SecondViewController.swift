e06084c1d278aa312c88072aa90771e57f2bec19
Fabian Vergara <vergarf@gmail.com>
2016-05-14 18:34:06 -0400

    Changed second view layout

--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController{
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +20,10 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+
+    
 
 
 }
