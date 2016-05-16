12e399230d4f6ccbb1d244804b88d7bf727e594f
Fabian Vergara <vergarf@gmail.com>
2016-05-16 12:56:39 -0400

    Implementation of a tableview to display our ToDoItem objects Small improvements in our code

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var items = [ToDoItem]()
+
+class FirstViewController: UIViewController, UITableViewDelegate {
+    
+    let emptyTable = 3
+  //  var items = [ToDoItem]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let userDefaults = NSUserDefaults.standardUserDefaults()
-        userDefaults.removeObjectForKey("itemsArray")
+        
+        items = retrieveItemList()!
+      //  let userDefaults = NSUserDefaults.standardUserDefaults()
+       // userDefaults.removeObjectForKey("itemsArray")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default,reuseIdentifier: "Cell")
+        
+
+        cell.textLabel!.text = "test"
+
+
+
+        return cell
+    }
+    
+    func retrieveItemList() -> [ToDoItem]? {
+        if let unarchivedObject = NSUserDefaults.standardUserDefaults().objectForKey("itemsArray") as? NSData {
+            return NSKeyedUnarchiver.unarchiveObjectWithData(unarchivedObject) as? [ToDoItem]
+        }else{
+            return items
+        }
     }
 
 
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -13,7 +13,7 @@ class SecondViewController: UIViewController, UITextFieldDelegate{
     @IBOutlet var tittleItem: UITextField!
     @IBOutlet var descItem: UITextField!
     
-    var items = [ToDoItem]()
+  //  var items = [ToDoItem]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
